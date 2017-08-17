7facda5170ee4898768fa78bccb1801c1cccce2d
balau123 <thai.itplus@gmail.com>
2017-08-17 20:00:40 +0700

    up

--- git_ctf.sh
+++ git_ctf.sh
@@ -2,9 +2,16 @@
 
 path="https://github.com/balau123/CTF/"
 username="balau123"
+email="thai.itplus@gmail.com"
+
 echo "username: " $username
 echo "path: " $path
+echo "email: " $email
 echo
+git config --global user.name $username
+git config --global user.email $email
+git config --global color.ui auto
+
 git add *
 git commit -m "up"
 git remote remove origin
